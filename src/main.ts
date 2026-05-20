2b3e6e30bce35deebaffefae724b5272ad2f26dd
luminuszz <luminuszz43@gmail.com>
2026-05-20 11:49:24 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from "./app.module";
 (async () => {
 	const app = await NestFactory.create(AppModule);
 
+	app.enableCors({
+		origin: ["http://localhost:1420", "tauri://localhost"],
+		methods: ["GET", "POST", "PUT", "PATCH"],
+	});
+
 	const env = app.get(ConfigService);
 
 	await app.listen(env.get("PORT") ?? 3000);
